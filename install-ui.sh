f10ab56ce43206205648220b9e4cec91e01699c6
Łukasz Magiera <magik6k@gmail.com>
2017-09-05 22:46:30 +0200

    polybar; fixup zsh keys

--- install-ui.sh
+++ install-ui.sh
@@ -3,5 +3,9 @@ DOTFILES=$HOME/dotfiles
 
 mkdir -p $HOME/.config/sxhkd
 mkdir -p $HOME/.config/bspwm
+mkdir -p $HOME/.config/polybar
+
 ln -sf "$DOTFILES/sxhkdrc" "$HOME/.config/sxhkd/sxhkdrc"
 ln -sf "$DOTFILES/bspwmrc" "$HOME/.config/bspwm/bspwmrc"
+ln -sf "$DOTFILES/polybar" "$HOME/.config/polybar/config"
+
